7f1cd27d8e9ca64282f2809e9f71e6c3fb4affe6
Michael Foerster <Michael.Foerster@com-science.de>
2018-12-12 22:58:49 +0100

    express app example

---
+++ myapp/app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+var httpServer = require('http').Server(app);
+
+httpServer.listen(3001, function() {
+  console.log("HTTP server runs on port 3001")
+})
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  var message = "Hallo Welt!";
+  res.render("index", {title:"Home", message:message})
+});
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
