ef0f871083453dbaedbc26f9ca5f8583213348a2
Michael Foerster <Michael.Foerster@com-science.de>
2018-12-15 23:15:13 +0100

    Extend websocket echo example

--- HelloEcho/echo-server.go
+++ HelloEcho/echo-server.go
@@ -4,22 +4,39 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"golang.org/x/net/websocket"
 )
 
-// Echo the data received on the WebSocket.
-func EchoServer(ws *websocket.Conn) {
+// echoServer serves websocket requests
+func echoServer(ws *websocket.Conn) {
 	fmt.Println("local address:", ws.LocalAddr())
 	fmt.Println("remote address:", ws.RemoteAddr())
-	io.Copy(ws, ws)
+	buf := make([]byte, 4096)
+	n, err := ws.Read(buf)
+	if err != nil {
+		fmt.Println("Could not read from websocket", err)
+	}
+	log.Printf("Received %d bytes: %q\n", n, buf[:n])
+	ws.Write([]byte("A"))
+}
+
+func handleMainRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("handled")
+	fc, err := ioutil.ReadFile("echo-client.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(fc)
 }
 
 // This example demonstrates a trivial echo server.
 func main() {
-	http.Handle("/echo", websocket.Handler(EchoServer))
+	http.HandleFunc("/", handleMainRoute)
+	http.Handle("/echo", websocket.Handler(echoServer))
 	err := http.ListenAndServe(":12345", nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
